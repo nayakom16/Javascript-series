d6b85e5aedbfafbc70f194aa6d68129892b3c363
nayakom16 <nayakankush71@gmail.com>
2024-06-11 23:06:23 +0530

    day-6

---
+++ day-6/1106/main.js
@@ -0,0 +1,10 @@
+// Q. Write a function findMax that takes an array of number as input and returns the maximum number in the array.
+
+const findMax = (arr)=>{
+    return Math.max(...arr)
+}
+
+
+// console.log(findMax([1,2,4,6,8]));
+// console.log(findMax([-1,-2,-4,-6,-8]));
+// console.log(findMax([5]));
