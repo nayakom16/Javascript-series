45c0a35a22398e28247e9dd62e8205af1e45b886
nayakom16 <nayakankush71@gmail.com>
2024-06-14 22:59:58 +0530

    day-9

---
+++ day-9/1406/main.js
@@ -0,0 +1,15 @@
+// Q. Write a function arrayAreEqual that takes two arrays arr1 and arr2 as input and returns true if the arrays are arrayAreEqual(1.e, contains in tthe same order),and false otherwise.
+
+const arrayAreEqual = (arr1, arr2)=>{
+    if(arr1.length ===  arr2.length){
+
+      return arr1.every((e , index) => e === arr2[index])
+    
+    }
+}
+
+
+
+// console.log(arrayAreEqual([1,2,3] , [1,2,3]));
+// console.log(arrayAreEqual([1,2,3] , [1,2,4]));
+// console.log(arrayAreEqual([] , []));
